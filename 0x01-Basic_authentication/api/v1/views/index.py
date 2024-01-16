9ded79d7315b46f8f77d10e5c7699b21bb12b6c2
damian oguche <damianguch@yahoo.com>
2024-01-16 18:08:23 +0100

    api/v1/views/index.py and app.py file modified

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,14 +27,18 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauth():
+def unauthorized() -> str:
     """ GET /api/v1/unauthorized
+    Return:
+      - raises a 401 error by using abort
     """
-    return abort(401)
+    abort(401)
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden():
+def forbidden() -> str:
     """ GET /api/v1/forbidden
+    Return:
+      - raises a 403 error by using abort
     """
-    return abort(403)
+    abort(403)
